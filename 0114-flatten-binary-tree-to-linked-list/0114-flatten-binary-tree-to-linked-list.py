d73fd109740486f114c060d7aa8dbb74135a9fb6
shraddhakuntoji53-del <shraddhakuntoji53@gmail.com>
2026-02-17 19:36:35 +0530

    Time: 0 ms (100%), Space: 19.5 MB (54.02%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -1,15 +1,15 @@
 class Solution:
-    def flatten(self, root: Optional[TreeNode]) -> None:
+    def flatten(self, root):
         curr = root
         
         while curr:
             if curr.left:
-                # Find rightmost node of left subtree
+                # find rightmost node of left subtree
                 prev = curr.left
                 while prev.right:
                     prev = prev.right
                 
-                # Rewire connections
+                # rewire pointers
                 prev.right = curr.right
                 curr.right = curr.left
                 curr.left = None
